dba6f91b3e78c517de2609583df5965766f73bbb
Crowdbotics <team@crowdbotics.com>
2020-07-28 17:04:16 +0000

    Update models

--- animation_editor_19221/wsgi.py
+++ animation_editor_19221/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'animation_editor_19221.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "animation_editor_19221.settings")
 
 application = get_wsgi_application()
